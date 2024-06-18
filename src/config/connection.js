212903945dad5bb994f1b8542c468a57ca2245f7
Andre Gregori Sangari <andresangari12@gmail.com>
2024-06-19 02:17:20 +0700

    add bucket

--- src/config/connection.js
+++ src/config/connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbPool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
-  //password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
